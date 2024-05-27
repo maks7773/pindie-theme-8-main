e0fd03c165512d25649be0d34ccd9bf835118842
maks7773 <149567240+maks7773@users.noreply.github.com>
2024-05-27 20:36:28 +0300

    mydomen

--- app/api/config.js
+++ app/api/config.js
@@ -1,5 +1,6 @@
-export const BASE_URL = 'http://localhost:3001/api'
 
+
+export const BASE_URL = 'https://mydomen.nomoredomainswork.ru/api'
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
