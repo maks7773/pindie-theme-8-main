5aa25a98461f92cc7b3b161dd26e903790ed5f2e
maks7773 <149567240+maks7773@users.noreply.github.com>
2024-05-29 15:38:39 +0300

    <сообщение коммита>

--- app/api/config.js
+++ app/api/config.js
@@ -1,6 +1,6 @@
 
 
-export const BASE_URL = 'https://mydomen.nomoredomainswork.ru'
+export const BASE_URL = 'https://mydomen.nomoredomainswork.ru/api'
 export const endpoints = {
   games: `${BASE_URL}/games/`,
   auth: `${BASE_URL}/auth/login/`,
